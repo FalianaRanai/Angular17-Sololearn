a8424e7fdb189bfbb44b350d709b498968048053
Faliana <faliana23@gmail.com>
2024-02-02 10:15:13 +0300

    init

---
+++ src/app/notes-list/notes-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NOTES } from './note';
+
+@Component({
+  selector: 'app-notes-list',
+  templateUrl: './notes-list.component.html',
+  styleUrls: ['./notes-list.component.css']
+})
+export class NotesListComponent {
+  listeNote = NOTES;
+}
