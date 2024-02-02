6cbc0bcdb0137c52cc2227e6ab328656ae214e7e
Faliana <faliana23@gmail.com>
2024-02-02 11:43:08 +0300

    arrangement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
   isRed = false;
+  name=""
+  username="test@email.com"
+  password="azerty"
 
-  login() {
+  welcome() {
     alert('Welcome!');
   }
 
   change(){
     this.isRed = !this.isRed;
+  }
+
+  showName(){
+    alert(this.name);
   }
 
 }
